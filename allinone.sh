9ba3e5753cb36f74af493c9fb60a8c3bca7c8a8d
ubun222 <925966086@qq.com>
2020-10-03 15:50:55 +0800

    support one-line allinone with txts.sh using "|"

--- allinone.sh
+++ allinone.sh
@@ -6,25 +6,20 @@ do
 
 read -p "请拖入!一个!文件回车,完成后请输入+号"  txt1
 
-if [[ "$txt1" != "+" ]];then
+if [[ "$txt1" = "+" ]];then
 
-echo $txt1
+break
+
+fi
 
 alltxt="$alltxt $(echo $(cat "$txt1" | grep -B 100 '\\' | sed 's/\\//g' ))"
 #有bug。alltxt="$alltxt$(echo $(cat $txt1 | awk -F'\\' '{printf $1}'))"
 
+done
+
 echo $alltxt
- 
-
-elif [[ "$txt1" = "+" ]];then
-
-clear
-
-#echo $alltxt | tr ' ' '\n' | sed 'N;s/\n/ /'
 
 echo  "creating $(pwd)/allinone.txt..."
-
-echo  
 
 read -p "type yes to continue..." yes
 
@@ -32,18 +27,21 @@ if  [[ "$yes" = "yes" ]];then
 
 echo $alltxt | tr ' ' '\n' | sed 'N;s/\n/ /' >./allinone.txt
 
+
+if [[ "$?" = "0" ]] ;then
+
+echo 50%finished... 
+
+fi
+
+fi
+
+
 echo "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" | cat >> ./allinone.txt
 
 if [[ "$?" = "0" ]] ;then
 
 echo Success! 
 
-break 
-
 fi
 
-fi
-
-fi
-
-done
---
+++ txts.sh
@@ -0,0 +1,5 @@
+echo $(dirname $0)/txt/9-3.txt 
+echo $(dirname $0)/txt/9-4.txt 
+echo $(dirname $0)/txt/9-9.txt
+echo + 
+echo yes
