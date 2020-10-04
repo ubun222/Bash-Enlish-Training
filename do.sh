575eb978704cf5361f1820b41fa16a09fe3cc0f8
ubun222 <925966086@qq.com>
2020-10-05 07:33:37 +0800

    one line push

--- do.sh
+++ do.sh
@@ -1,1 +1,1 @@
-cd $(dirname $0) && echo $(date) > date.txt && git add date.txt && git commit -m "do origin/master"
+cd $(dirname $0) && echo $(date) > date.txt && git add date.txt && git commit -m "do origin/master" && git push origin do
