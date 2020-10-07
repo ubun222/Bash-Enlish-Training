aae92038d1315b4f6cb0cb376703256934e69f39
ubun222 <925966086@qq.com>
2020-10-07 10:15:15 +0800

    rmadd

--- 1.0.6.sh
+++ 1.0.6.sh
@@ -1,5 +1,5 @@
-read -r -d '\' txt1 < $1 && read -r -d '\' txt2 < $2 && read -r -d '\' txt3 < $3
-txt=$(echo $txt1 && echo $txt2 && echo $txt3)
+((read -r -d '\' txt1 < $1  && read -r -d '\' txt2 < $2 && read -r -d '\' txt3 < $3) >/dev/tty) >&errors
+txt=$( echo $txt1 && echo $txt2 && echo $txt3)
 n=$(echo $txt | awk 'BEGIN{RS=" "}{print FNR}' | sed -n '$p')
 echo 检测到$[$n/2]组单词
 m=$(($n+0))
